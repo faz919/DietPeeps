31e32c1d309e9bec64c714cbd915590c473cfbb3
faz919 <faizit9@gmail.com>
2022-04-09 00:00:29 +0300

    18x9 vs 16x9 logic

--- screens/IntroVideo.js
+++ screens/IntroVideo.js
@@ -1,36 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import { StyleSheet, TouchableOpacity, Text, View } from 'react-native'
 import Video from 'react-native-video'
+import { windowHeight, windowWidth } from '../utils/Dimensions'
 
 const IntroVideo = ({ navigation }) => {
-    
+
     const [videoPlaying, setVideoPlaying] = useState(false)
     const [showSkip, setShowSkip] = useState(false)
 
     useEffect(() => {
-        videoPlaying? setTimeout(() => {
+        videoPlaying ? setTimeout(() => {
             setShowSkip(true)
         }, 10000) : null
     }, [videoPlaying])
 
     return (
-        <View style={{flex: 1, backgroundColor: '#e6e7fa'}}>
-        <Video 
-            source={require('../assets/intro-video.mp4')} 
-            style={styles.introVideo}
-            fullscreen={true}
-            fullscreenOrientation={'portrait'}
-            fullscreenAutorotate={false}
-            onLoad={() => setVideoPlaying(true)}
-            onEnd={() => navigation.replace('Onboarding')}
-            resizeMode='contain'
-            ignoreSilentSwitch='ignore'
-        />
-        {showSkip ? 
-            <TouchableOpacity style={styles.panelButton} onPress={() => navigation.replace('Onboarding')}>
-                <Text style={styles.panelButtonTitle}>{'Skip'}</Text>
-            </TouchableOpacity>
-        : null}
+        <View style={{ flex: 1, backgroundColor: '#e6e7fa' }}>
+            {windowHeight / windowWidth === 2 ?
+                <Video
+                    source={require('../assets/intro-video-18-9.mp4')}
+                    style={styles.introVideo}
+                    fullscreen={true}
+                    fullscreenOrientation={'portrait'}
+                    fullscreenAutorotate={false}
+                    onLoad={() => setVideoPlaying(true)}
+                    onEnd={() => navigation.replace('Onboarding')}
+                    resizeMode='contain'
+                    ignoreSilentSwitch='ignore'
+                />
+                :
+                <Video
+                    source={require('../assets/intro-video-16-9.mp4')}
+                    style={styles.introVideo}
+                    fullscreen={true}
+                    fullscreenOrientation={'portrait'}
+                    fullscreenAutorotate={false}
+                    onLoad={() => setVideoPlaying(true)}
+                    onEnd={() => navigation.replace('Onboarding')}
+                    resizeMode='contain'
+                    ignoreSilentSwitch='ignore'
+                />
+            }
+            {showSkip ?
+                <TouchableOpacity style={styles.panelButton} onPress={() => navigation.replace('Onboarding')}>
+                    <Text style={styles.panelButtonTitle}>{'Skip'}</Text>
+                </TouchableOpacity>
+                : null}
         </View>
     )
 }
